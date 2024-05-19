93528a22cf97a2818f423be6c7ca159ff022d91f
Sachin <heysachinkry@gmail.com>
2024-05-19 21:04:36 +0530

    few more changes....

---
+++ app/legal/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PrivacyPolicy: React.FC = () => (
+  <div className="p-6 max-w-4xl mx-auto font-mono">
+    <div className="text-center text-xl font-bold mb-4">Our Privacy Policy</div>
+    <div className="uppercase text-center my-16">.........To have fun.........</div>
+    <div className="text-center">
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="text-center text-purple-600 text-xl underline">Show more...</a>
+    </div>
+  </div>
+);
+
+export default PrivacyPolicy;
---
+++ app/legal/terms-of-service/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react';
+
+const TermsOfService: React.FC = () => (
+  
+  <div className="p-6 max-w-4xl mx-auto font-mono text-xs">
+    <h1 className="text-xl font-bold mb-4 text-center">Terms of Service</h1>
+    <div className="uppercase text-center my-16">.........To have fun.........</div>
+    <div className="text-center">
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="text-center text-purple-600 text-xl underline">Show more..</a>
+    </div>
+  </div>
+);
+
+export default TermsOfService;
